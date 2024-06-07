eaf51f76befe75e56bebb00526693994877fd61f
LSigala <lsigala@arkusnexus.com>
2024-06-07 11:14:31 -0600

    Adding NavBar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import 'bootstrap/dist/css/bootstrap.css';
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="">
+          <nav className="navbar navbar-expand-lg p-2 navbar-light bg-secondary">
+            <a className="navbar-brand" href="http://localhost:3000/">ArkusNexus</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item active">
+                  <a className="navbar-brand" href="http://localhost:3000/Vacant">Vacant</a>
+                </li>
+                <li className="nav-item">
+                  <a className="navbar-brand" href="http://localhost:3000/Consult">Consult</a>
+                </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
+
+        {children}
+        
+        </body>
     </html>
   )
 }
