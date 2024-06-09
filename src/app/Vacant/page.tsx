67b61130a72acbc22cac4a855f59e7d5c7608df2
LSigala <lsigala@arkusnexus.com>
2024-06-08 20:19:23 -0600

    Adding process to update Vacant and paginated

--- src/app/Vacant/page.tsx
+++ src/app/Vacant/page.tsx
@@ -5,14 +5,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { useState } from "react";
 import { useEffect } from "react";
 import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 export default function Home() {
 
   const [Data, setdata] = useState();
-  const [showMe, setShowMe] = useState(false);
-  function toggle(){
-    setShowMe(!showMe);
-  }
+  const [ID, setID] = useState();
+  const [show, setShow] = useState(false);
+  const [Page, setPage] = useState(0);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     ObtainData();
@@ -20,48 +24,79 @@ export default function Home() {
   } , []);
 
   async function ObtainData(){
-    const response = await axios.get("http://localhost:3001/vacancies/")
+    const response = await axios.post("http://localhost:3001/vacancies/obtainData")
     const responseData = response.data;
+    
     setdata(responseData);
+  }
+
+  async function handleId(id: any){
+    handleShow()
+    setID(id)
+
   }
 
   async function SendData(e: any){
     e.preventDefault();
 
-    const data = {
-      vacancy_status: String(e.target.vacancy_status.value),
-      id: String(e.target.id.value)
-    }
-
-    console.log(data.vacancy_status);
-    console.log(data.id);
+    const vacancy_Status = document.getElementById("vacancy_status") as HTMLInputElement;
+    const vacancy_status = vacancy_Status.value;
       
-   /*  try {
-      const response = await axios.put(`http://localhost:3001/vacancies/${data.id}`, {
-        vacancy_status: data.vacancy_status
+    try {
+      const response = await axios.put(`http://localhost:3001/vacancies/${ID}`, {
+        vacancy_status: vacancy_status
       })
-      console.log(response)
+      window.location.reload();
     } catch (error) {
       alert(error);
-    } */
+    }
+    
+  }
+
+  async function next(){
+    setPage(Page + 2)
+    let skip = Page
+    if(Page != 0){
+      const response = await axios.post("http://localhost:3001/vacancies/obtainData", { skip: skip })
+      if(response.data.length == 0){
+        setPage(Page - 2)
+        const response = await axios.post("http://localhost:3001/vacancies/obtainData", { skip: skip })
+        const responseData = response.data;
+        setdata(responseData);
+        window.scrollTo({ top: 0, behavior: 'instant' });
+        }else{
+        const responseData = response.data;
+        setdata(responseData);
+        window.scrollTo({ top: 0, behavior: 'instant' });
+      }
+    }
+  }
+
+  async function previous(){
+    if(Page != 0){
+      setPage(Page - 2)
+      let skip = Page
+      const response = await axios.post("http://localhost:3001/vacancies/obtainData", { skip: skip })
+      const responseData = response.data;
+      setdata(responseData);
+      window.scrollTo({ top: 0, behavior: 'instant' });
+    }else{
+      let skip = 0
+      const response = await axios.post("http://localhost:3001/vacancies/obtainData", { skip: skip })
+      const responseData = response.data;
+      setdata(responseData);
+      window.scrollTo({ top: 0, behavior: 'instant' });
+    }
     
   }
 
   return (
-    <div onSubmit={SendData} className="container mt-4">
+    <div className="container mt-4">
       {Data?.map((data) => (
       <div key={data.id} id="id" className="card mb-4">
         <div className="card-header">
-          <div style={{display: showMe?"none":"block"}}> 
+          <div onClick={() => handleId(data.id)}> 
             <button type="button" className="btn btn-success col-2">{data.vacancy_status}</button>
-          </div>
-          <div className="form-group" style={{display: showMe?"block":"none"}}>
-            <label>Cambiar estatus</label>
-            <select className="form-control col-md-2 mb-3" id="vacancy_status" required>
-              <option>IN_PROCESS</option>
-              <option>AVAILABLE</option>
-              <option>TAKEN</option>
-            </select>
           </div>
         </div>
         <div className="card-body">
@@ -88,10 +123,35 @@ export default function Home() {
         </div>
       </div>
   ))}
+      <Modal className="mt-5" show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Elija el estatus de la vacante</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+        <form id="SendData" onSubmit={SendData}>
+          <div className="form-group">
+            <label>Cambiar estatus</label>
+            <select className="form-control col-md-2 mb-3" id="vacancy_status" required>
+              <option>IN_PROCESS</option>
+              <option>AVAILABLE</option>
+              <option>TAKEN</option>
+            </select>
+          </div>
+        </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Cerrar
+          </Button>
+          <Button variant="primary" type="submit" form="SendData" onClick={handleClose}>
+            Guardar cambios
+          </Button>
+        </Modal.Footer>
+      </Modal>
       
           <div className="d-flex justify-content-center">
-            <button type="submit" onClick={toggle} className="btn btn-dark mt-3 mb-3 mx-2">Editar</button>
-            <button type="submit" onClick={SendData} style={{display: showMe?"block":"none"}} className="btn btn-dark mt-3 mb-3 mx-2">Guardar</button>
+            <button type="submit" onClick={previous} className="btn btn-dark mt-3 mb-3 mx-2">Anterior</button>
+            <button type="submit" onClick={next} className="btn btn-dark mt-3 mb-3 mx-2">Siguiente</button>
             </div>
     </div>
   );
