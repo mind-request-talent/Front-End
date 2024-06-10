0a24167fc45b48a9301132589e87a71555e76551
LSigala <lsigala@arkusnexus.com>
2024-06-10 15:00:50 -0600

    Updating urls for deploy website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,17 +20,17 @@ export default function RootLayout({
       <body className={inter.className}>
         <div className="">
           <nav className="navbar navbar-expand-lg p-2 navbar-light bg-secondary">
-            <a className="navbar-brand" href="http://localhost:3000/">ArkusNexus</a>
+            <a className="navbar-brand" href="https://front-end-210z.onrender.com/">ArkusNexus</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item active">
-                  <a className="navbar-brand" href="http://localhost:3000/Vacant">Vacant</a>
+                  <a className="navbar-brand" href="https://front-end-210z.onrender.com/Vacant">Vacant</a>
                 </li>
                 <li className="nav-item">
-                  <a className="navbar-brand" href="http://localhost:3000/Consult">Consult</a>
+                  <a className="navbar-brand" href="https://front-end-210z.onrender.com/Consult">Consult</a>
                 </li>
               </ul>
             </div>
